e411e37722fc3980eb7f7a9fb0d460a1342b7fe6
LAPTOP-DQT65H7B\User <sha2672tlpu@gmail.com>
2023-06-20 12:46:13 +0800

    init

---
+++ src/main/java/com/example/questionnaire_back/QuestionnaireBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionnaire_back/QuestionnaireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnaire_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
